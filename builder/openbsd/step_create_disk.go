740839de2dc36f5de88ff1fbd2ae0c84b4084143
niamtokik <niamtokik@localhost>
2020-02-07 16:25:35 +0000

    to test

--- builder/openbsd/step_create_disk.go
+++ builder/openbsd/step_create_disk.go
@@ -14,16 +14,50 @@ type stepCreateDisk struct{}
 
 func (s *stepCreateDisk) Run(ctx context.Context, state multistep.StateBag) multistep.StepAction {
 	config := state.Get("config").(*Config)
-	// _ := state.Get("driver").(Driver)
+	driver := state.Get("driver").(Driver)
 	ui := state.Get("ui").(packer.Ui)
 	name := config.VMName
 
+	if config.DiskImage && !config.UseBackingFile {
+		return multistep.ActionContinue
+	}
+	
 	var diskFullPaths, diskSizes []string
 
 	ui.Say("Creating required virtual machine disks")
-	diskFullPaths = append(diskFullPaths, filepath.Join("config.OutputDir", name))
+	// The 'main' or 'default' disk
+	diskFullPaths = append(diskFullPaths, filepath.Join(config.OutputDir, name))
 	diskSizes = append(diskSizes, fmt.Sprintf("%s", config.DiskSize))
+	
+	// Additional disks
+	if len(config.AdditionalDiskSize) > 0 {
+		for i, diskSize := range config.AdditionalDiskSize {
+			path := filepath.Join(config.OutputDir, fmt.Sprintf("%s-%d", name, i+1))
+			diskFullPaths = append(diskFullPaths, path)
+			size := fmt.Sprintf("%s", diskSize)
+			diskSizes = append(diskSizes, size)
+		}
+	}
 
+	for i, diskFullPath := range diskFullPaths {
+		log.Printf("[INFO] Creating disk with Path: %s and Size: %s", diskFullPath, diskSizes[i])
+		command := []string{
+			"create",
+			"-s",
+			diskSizes[i],
+			diskFullPath,
+		}
+
+		if err := driver.Vmctl(command...); err != nil {
+			err := fmt.Errorf("Error creating hard drive: %s", err)
+			state.Put("error", err)
+			ui.Error(err.Error())
+			return multistep.ActionHalt
+		}
+	}
+
+	state.Put("vmctl_disk_paths", diskFullPaths)
+	
 	return multistep.ActionContinue
 }
 
